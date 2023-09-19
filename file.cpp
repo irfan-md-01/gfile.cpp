0ff0ca413495044b2ba4d2cc5e338110b5a67625
irfan-md-01 <145257683+irfan-md-01@users.noreply.github.com>
2023-09-19 17:37:02 +0530

    file.cpp

--- file.cpp
+++ file.cpp
@@ -1,27 +1,18 @@
 #include <iostream>
-#include <cmath>
-#include <algorithm> 
 using namespace std;
 
 long double power(double, int );
-long double power(double, long int =2);
+//long double power(double, long int =2);
 
 int main() {
     double m;
     int n;
     cin >> m;
-    cout << power(m) << endl;
     cin >> n;
     cout << power(m, n);
-   
-
 
 long double power(double m, int n) {
     return pow(m, n);
 }
-
-long double power(double m, long int n) {
-    return pow(m, n);
-
   return 0;
 }
